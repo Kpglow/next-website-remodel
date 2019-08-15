78adac64965fab0c5ce5348a8c0e2701eef29f96
Karl <karlwng@gmail.com>
2019-08-14 23:17:48 -0700

    removed settings heroku bug

--- next.config.js
+++ next.config.js
@@ -1,12 +1,11 @@
 const path = require('path')
 const glob = require('glob')
-const { parsed: localEnv } = require('dotenv').config()
-const webpack = require('webpack')
+
 
 module.exports = {
   
   webpack: (config, { dev }) => {
-    config.plugins.push(new webpack.EnvironmentPlugin(localEnv))
+
     config.module.rules.push(
       {
         test: /\.(css|scss)/,
@@ -36,9 +35,7 @@ module.exports = {
     )
     return config
   },
-  publicRuntimeConfig: {
-    AWS_URL: process.env.AWS_URL
-}
+
   // exportPathMap: function(defaultPathMap) {
   //   return {
   //     '/': { page: '/' }
